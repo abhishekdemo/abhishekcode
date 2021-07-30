a25e4592d8e9c412aa19e8804d05062d251e65a1
abhishekdemo <88167703+abhishekdemo@users.noreply.github.com>
2021-07-30 19:22:26 +0530

    Add files via upload

---
+++ exam4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main() {
+   int pipefds[2];
+   int returnstatus;
+   int pid;
+   char writemessages[2][20]={"my name is","mukesh"};
+   char readmessage[20];
+   returnstatus = pipe(pipefds);
+   if (returnstatus == -1) {
+      printf("Unable to create pipe\n");
+      return 1;
+   }
+   pid = fork();
+   
+   // Child process
+   if (pid == 0) {
+      read(pipefds[0], readmessage, sizeof(readmessage));
+      printf("Child Process - Reading from pipe – Message 1 is %s\n", readmessage);
+      read(pipefds[0], readmessage, sizeof(readmessage));
+      printf("Child Process - Reading from pipe – Message 2 is %s\n", readmessage);
+   } else { //Parent process
+      printf("Parent Process - Writing to pipe - Message 1 is %s\n", writemessages[0]);
+      write(pipefds[1], writemessages[0], sizeof(writemessages[0]));
+      printf("Parent Process - Writing to pipe - Message 2 is %s\n", writemessages[1]);
+      write(pipefds[1], writemessages[1], sizeof(writemessages[1]));
+      write(writemessages,sizeof(char),strlen(writemessages));
+   }
+   return 0;
+}
